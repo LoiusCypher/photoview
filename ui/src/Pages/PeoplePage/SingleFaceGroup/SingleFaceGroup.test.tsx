43377a144ba5c8c7651f425349b7c330dee0b3a2
LoiusCypher <loius@github.knirsch.de>
2025-11-15 06:09:22 +0100

    First try to upgrade history test cases, try 96

--- ui/src/Pages/PeoplePage/SingleFaceGroup/SingleFaceGroup.test.tsx
+++ ui/src/Pages/PeoplePage/SingleFaceGroup/SingleFaceGroup.test.tsx
@@ -96,64 +96,23 @@ test('single face group', async () => {
             id: '2',
             label: 'Face Group Name',
             imageFaces: [
-              {
-                __typename: 'ImageFace',
-                id: '1',
-                rectangle: {
-                  __typename: 'FaceRectangle',
-                  minX: 0.4912109971046448,
-                  maxX: 0.5927730202674866,
-                  minY: 0.2998049855232239,
-                  maxY: 0.4013670086860657,
-                },
-                media: {
-                  __typename: 'Media',
-                  id: '10',
-                  type: 'Photo',
-                  title: '122A2785-2.jpg',
-                  blurhash: 'LeIEnWD%n$Rj~pRiM{ofIpW=Iot6',
-                  thumbnail: {
-                    __typename: 'MediaURL',
-                    url: '/photo/thumbnail_122A2785-2_jpg_lFmZcaN5.jpg',
-                    width: 1024,
-                    height: 1024,
-                  },
-                  highRes: {
-                    __typename: 'MediaURL',
-                    url: '/photo/122A2785-2_e4nCeMHU.jpg',
-                  },
-                  favorite: false,
-                },
-              },
-              {
-                __typename: 'ImageFace',
-                id: '2',
-                rectangle: {
-                  __typename: 'FaceRectangle',
-                  minX: 0.265625,
-                  maxX: 0.3876950144767761,
-                  minY: 0.1917019933462143,
-                  maxY: 0.3705289959907532,
-                },
-                media: {
-                  __typename: 'Media',
-                  id: '52',
-                  type: 'Photo',
-                  title: 'image.png',
-                  blurhash: 'LeIEnWD%n$Rj~pRiM{ofIpW=Iot6',
-                  thumbnail: {
-                    __typename: 'MediaURL',
-                    url: '/photo/thumbnail_image_png_OwTDG5fM.jpg',
-                    width: 1024,
-                    height: 699,
-                  },
-                  highRes: {
-                    __typename: 'MediaURL',
-                    url: '/photo/image_A2YB0x3z.png',
-                  },
-                  favorite: false,
-                },
-              },
+            ],
+          },
+        },
+      },
+    },
+    {
+      request: {
+        query: SINGLE_FACE_GROUP,
+        variables: { },
+      },
+      result: {
+        data: {
+          faceGroup: {
+            __typename: 'FaceGroup',
+            id: '2',
+            label: 'Face Group Name',
+            imageFaces: [
             ],
           },
         },
