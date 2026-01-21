5f71a3bf3246e83107e8b498a1280d80f5fd0eee
LoiusCypher <loius@github.knirsch.de>
2026-01-21 17:03:28 +0100

    Adjust face classification threshold from 0.2 to 0.1

--- api/scanner/face_detection/face_detector_impl.go
+++ api/scanner/face_detection/face_detector_impl.go
@@ -128,7 +128,7 @@ func (fd *faceDetector) DetectFaces(db *gorm.DB, media *models.Media) error {
 }
 
 func (fd *faceDetector) classifyDescriptor(descriptor face.Descriptor) int32 {
-	return int32(fd.rec.ClassifyThreshold(descriptor, 0.2))
+	return int32(fd.rec.ClassifyThreshold(descriptor, 0.1))
 }
 
 func (fd *faceDetector) classifyFace(db *gorm.DB, face *face.Face, media *models.Media, imagePath string) error {
